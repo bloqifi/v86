e45b7689d52ba63145e0d84d718e6fd603e177b3
Kris Henriksen <khe@bloqifi.com>
2022-12-03 03:01:36 +0700

    change wait to sleep

--- tools/docker/ubuntu/networking.sh
+++ tools/docker/ubuntu/networking.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rmmod ne2k-pci
 modprobe ne2k-pci
-wait
+sleep 1
 INF=`ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' | head -n 1`
 ifconfig $INF hw ether 00:22:15:19:01:25
 ifconfig $INF 10.5.225.253 netmask 255.255.0.0 up
