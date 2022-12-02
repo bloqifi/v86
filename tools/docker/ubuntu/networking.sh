4cb2c619bb840b5b69f0de2722a1f04ffe99f871
Kris Henriksen <khe@bloqifi.com>
2022-12-03 02:15:39 +0700

    set custom mac address

--- tools/docker/ubuntu/networking.sh
+++ tools/docker/ubuntu/networking.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 rmmod ne2k-pci
 modprobe ne2k-pci
-sleep 1
-ifconfig `ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' | head -n 1` 10.5.225.253 netmask 255.255.0.0 up
-
+wait
+INF=`ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' | head -n 1`
+ifconfig $INF hw ether 00:28:C7:0A:42:A2
+ifconfig $INF 10.5.225.253 netmask 255.255.0.0 up
 EXIST=`ip route show 10.5.0.1 | wc -l`
 if [ $EXIST -eq 0 ]
 then
