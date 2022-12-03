1bfd7516bf5b5869d30d3130a97b5d232a117645
Kris Henriksen <khe@bloqifi.com>
2022-12-04 01:44:58 +0700

    remove service file, to slow. remove static mac address

--- tools/docker/ubuntu/networking.sh
+++ tools/docker/ubuntu/networking.sh
@@ -3,7 +3,7 @@ rmmod ne2k-pci
 modprobe ne2k-pci
 sleep 1
 INF=`ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' | head -n 1`
-ifconfig $INF hw ether 00:22:15:19:01:25
+#ifconfig $INF hw ether 00:22:15:19:01:25
 ifconfig $INF 10.5.225.253 netmask 255.255.0.0 up
 EXIST=`ip route show 10.5.0.1 | wc -l`
 if [ $EXIST -eq 0 ]
